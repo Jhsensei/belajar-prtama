eead007c587f4816bec81b4ecc11d6aa1183c4f4
Soyaaa <haydarahmaddd@proton.me>
2025-01-17 02:31:21 +0700

    helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Belajar Pertama di RPN</h2>
+
+<p id="1">Perkenalkan nama saya Soyaa</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  document.getElementById("1").innerHTML = "Saya ingin belajar sungguh-sungguh";
+}
+</script>
+
+</body>
+</html> 
